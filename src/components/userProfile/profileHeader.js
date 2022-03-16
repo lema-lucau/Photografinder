eba3318eb19edc72ddd6e407caf06c76fdd97f7a
Lema Lucau <55754825+lema-lucau@users.noreply.github.com>
2022-03-16 21:59:10 +0000

    Display user details in header

--- src/components/userProfile/profileHeader.js
+++ src/components/userProfile/profileHeader.js
@@ -2,11 +2,13 @@ import { Modal } from "@mantine/core";
 import { useState, useEffect } from "react";
 import { LOGGED_IN_USER } from "../../constants/user";
 import { getUserByUserId } from "../../services/users";
+import { getAllPhotographerPosts } from "../../services/posts";
 import BookingForm from "../bookingForm";
 
 export default function ProfileHeader({user}) {
     let fbUser = JSON.parse(localStorage.getItem(LOGGED_IN_USER));
     const [loggedInUser, setLoggedInUser] = useState(null);
+    const [numPhotos, setNumPhotos] = useState(null);
 
     useEffect(() => {
         fbUser = JSON.parse(localStorage.getItem(LOGGED_IN_USER));
@@ -16,14 +18,16 @@ export default function ProfileHeader({user}) {
             .then(returnedUser => setLoggedInUser(returnedUser));
         }
 
+        const getNumPhotos = async () => {
+            await getAllPhotographerPosts(user.uid)
+            .then(posts => setNumPhotos(posts.length));
+        }
+
         getUser();
+        getNumPhotos();
     }, []);
 
     const [opened, setOpened] = useState(false);
-
-    const minRate = "00";
-    const numFollowers = "00";
-    const numPhotos = "00";
 
     const EditButton = () => {
         return(
@@ -90,28 +94,33 @@ export default function ProfileHeader({user}) {
     return(
         <div className="grid grid-cols-2 gap-4 border border-black px-12">
             {/* Profile info */}
-            <div className="flex flex-col justify-center py-6">
-                <p className="pt-2">Username</p>
-                <p className="font-bold pt-4">Full Name</p>
-                {/* Set character limit of 500  for about me section*/}
-                <p className="pt-4">This section is about me. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium quis risus id rhoncus. Nullam tincidunt rhoncus lacus ut porttitor. Suspendisse tincidunt magna eget venenatis sodales. Integer feugiat quis elit quis tempus. Aliquam aliquet facilisis metus, consequat consectetur massa cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec id ligula luctus augue consequat facilisis. Etiam in magna congue, egestas ipsum.</p>
-                <p className="pt-4">
-                    <span className="font-bold">Min rate: </span>
-                    &euro; {minRate}/hr
-                </p>
-                <p className="pt-4">
-                    <span className="font-bold">Location: </span>
-                    SnapIt Studios, Dublin 18, Co. Dublin, A12 BC34
-                </p>
-                <p className="pt-4">
-                    <span className="font-bold">{numPhotos}</span> Photos,
-                    <span className="font-bold"> {numFollowers}</span> {numFollowers === 1 ? 'Follower' : 'Followers'}
-                </p>
+            <div className="flex flex-col justify-around py-6">
+                <p className="text-lg italic pt-2">{user.username}</p>
+                <p className="font-bold pt-4">{`${user.firstName} ${user.lastName}`}</p>
+                <p className="pt-4">{user.bio}</p>
+
+                {user?.type === "Photographer" ?
+                    <>
+                        <p className="pt-4">
+                        <span className="font-bold">Min rate: </span>
+                        &euro;{user.minRate}/hr
+                        </p>
+                        <p className="pt-4">
+                            <span className="font-bold">Location: </span>
+                            {user.location}
+                        </p>
+                        <p className="pt-4">
+                            <span className="font-bold">{numPhotos}</span> Photos,
+                            <span className="font-bold"> {user.followers.length}</span> {user.followers.length === 1 ? 'Follower' : 'Followers'}
+                        </p>
+                    </>
+                    : null
+                }
             </div>
             {/* Profile picture and buttons */}
             <div className="flex flex-col items-end py-6">
                 <div className="flex flex-col items-center">
-                    <img className="w-44 h-44 object-cover rounded-full border-black" src="images/default_user_icon.png" alt="users profile pic"/>
+                    <img className="w-44 h-44 object-cover rounded-full border-black" src="../images/default_user_icon.png" alt="users profile pic"/>
                     {fbUser?.uid && loggedInUser?.uid ?
                         <>
                             <IsOwnProfile />
